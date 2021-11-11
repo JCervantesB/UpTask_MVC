7a03a8a85d1830b1ea23c5934bee41dee153d477
JCervantesB <pc3dtepic@gmail.com>
2021-11-10 17:06:50 -0700

    Terminado el menu Mobile

--- views/dashboard/perfil.php
+++ views/dashboard/perfil.php
@@ -1,5 +1,7 @@
 <?php include_once __DIR__ . '/header-dashboard.php' ?>
 
-
+<div class="contenedor-sm">
+    <?php include_once __DIR__ - '/../templates/alertas.php'; ?>
+</div>
 
 <?php include_once __DIR__ . '/footer-dashboard.php' ?>
--- views/templates/sidebar.php
+++ views/templates/sidebar.php
@@ -1,9 +1,18 @@
 <aside class="sidebar">
-    <h2>UpTask</h2>
+    <div class="contenedor-sidebar">
+        <h1>UpTask</h1>
+        <div class="cerrar-menu">
+            <img src="build/img/cerrar.svg" alt="imagen cerrar menu" id="cerrar-menu">
+        </div>
+    </div>
 
     <nav class="sidebar-nav">
         <a class="<?php echo ($titulo === 'Proyectos') ? 'activo' : ''; ?>" href="dashboard">Proyectos</a>
         <a class="<?php echo ($titulo === 'Crear Proyecto') ? 'activo' : ''; ?>" href="crear-proyecto">Crear proyectos</a>
         <a class="<?php echo ($titulo === 'Perfil') ? 'activo' : ''; ?>" href="perfil">Perfil</a>
     </nav>    
+
+    <div class="cerrar-sesion-mobile">
+        <a href="/logout" class="cerrar-sesion">Cerrar Sesión</a>
+    </div>
 </aside>
